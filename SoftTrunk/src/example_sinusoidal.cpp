197ff30fd4f23e9a0c8479dc70d690cc937f3d91
Yasu-gakkaPC <yasu313nori@gmail.com>
2018-10-18 11:22:33 -0400

    update examples for final arm

--- SoftTrunk/src/example_sinusoidal.cpp
+++ SoftTrunk/src/example_sinusoidal.cpp
@@ -9,14 +9,18 @@ sends a sinusoidal wave to 4 actuators, and each phase is offset by PI/2.
 */
 void wait() { std::this_thread::sleep_for(std::chrono::milliseconds(10)); }
 
-int sinusoid(double t) { return 500 + 400 * sin(t); }
+int sinusoid(double t) { return 200 + 200 * sin(t); }
 int main() {
-  ForceController forceController(4);
+  ForceController forceController(16);
 
   for (int i = 0; i < 10000; i++) {
     for (int j = 0; j < 4; j++) {
       forceController.setSinglePressure(
-          j, sinusoid(static_cast<double>(i) / 40 + PI * j / 2));
+          j+4, sinusoid(static_cast<double>(i) / 20 + PI * j / 2));
+      forceController.setSinglePressure(
+              j+8, sinusoid(static_cast<double>(i) / 20 + PI * j / 2 + PI * 2/3));
+      forceController.setSinglePressure(
+              j+12, sinusoid(static_cast<double>(i) / 20 + PI * j / 2 + PI * 4/3));
     }
     wait();
   }
