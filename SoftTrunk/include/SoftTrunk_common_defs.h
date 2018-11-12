81a5bec10d23eda563730c1a7d933ce5a67feb50
Yasu <yasu313nori@gmail.com>
2018-11-11 23:19:27 -0500

    feat: tries to keep constant control cycle feat: experiment curvature profile

--- SoftTrunk/include/SoftTrunk_common_defs.h
+++ SoftTrunk/include/SoftTrunk_common_defs.h
@@ -19,8 +19,9 @@
 #define PRESSURE_OFFSET 700 // baseline pressure of arm.
 #define MAX_PRESSURE 1300
 #define USE_PID_CURVATURE_CONTROL false
+#define USE_FEEDFORWARD_CONTROL true
 #define PI 3.141592
-#define CONTROL_PERIOD 0.0005 // period of one control step, in seconds.
+#define CONTROL_PERIOD 0.005 // period of one control step, in seconds. must be above 0.002??(verify) because that's how long a control loop takes
 
 typedef Eigen::Matrix<double,NUM_ELEMENTS*2,1> Vector2Nd;
 typedef Eigen::Matrix<double,NUM_ELEMENTS*2,NUM_ELEMENTS*2> Matrix2Nd;
--- SoftTrunk/src/experiment.cpp
+++ SoftTrunk/src/experiment.cpp
@@ -6,6 +6,7 @@
 #include <Eigen/Dense>
 #define DURATION 20
 
+
 #include "SoftTrunk_common_defs.h"
 
 int main(){
@@ -15,8 +16,14 @@ int main(){
     Vector2Nd ddq = Vector2Nd::Zero();
     double seconds;
 
-    for (double seconds  = 0; seconds < DURATION; seconds+=CONTROL_PERIOD) {
+    std::chrono::high_resolution_clock::time_point lastTime;
+    int duration;
+    int i;
+    long long sum = 0;
 
+    for (double seconds  = 0; seconds < DURATION; seconds+=CONTROL_PERIOD) {
+        i++;
+        lastTime = std::chrono::high_resolution_clock::now();
         // first set the commands
         // make sure phi are not near 0 (problems when crossing over 0)
         //todo: having phi go over phi=0 without hitch with PID controller
@@ -24,22 +31,40 @@ int main(){
         q(0) = PI + 1 * cos(a*seconds);
         dq(0) = -1 * a* sin(a*seconds);
         ddq(0) = -1 * a*a* cos(a*seconds);
+
+        q(2) = PI + 1 * cos(a*seconds+PI/3);
+        dq(2) = -1 * a* sin(a*seconds+PI/3);
+        ddq(2) = -1 * a*a* cos(a*seconds+PI/3);
+
+        q(4) = PI + 1 * cos(a*seconds+PI*2/3);
+        dq(4) = -1 * a* sin(a*seconds+PI*2/3);
+        ddq(4) = -1 * a*a* cos(a*seconds+PI*2/3);
+
         double startUpTime = 1;
-        double maxTheta = 0.3;
+        double maxTheta = 0.4;
         if (seconds < startUpTime){
             q(1) = maxTheta/2 - maxTheta/2 * cos(seconds*(PI/startUpTime));
             dq(1) = -maxTheta/2 * sin(seconds*(PI/startUpTime)) * (PI/startUpTime);
             ddq(1) = -maxTheta/2 * cos(seconds*(PI/startUpTime))* (PI*PI/(startUpTime*startUpTime));
-        }
-        else
-            q(1) = maxTheta;
-        q(2) = PI;
-        q(3) = 0.2;
 
-        q(4) = PI;
-        q(5) = 0.2;
+            q(3) = maxTheta/2 - maxTheta/2 * cos(seconds*(PI/startUpTime));
+            dq(3) = -maxTheta/2 * sin(seconds*(PI/startUpTime)) * (PI/startUpTime);
+            ddq(3) = -maxTheta/2 * cos(seconds*(PI/startUpTime))* (PI*PI/(startUpTime*startUpTime));
+
+            q(5) = maxTheta/2 - maxTheta/2 * cos(seconds*(PI/startUpTime));
+            dq(5) = -maxTheta/2 * sin(seconds*(PI/startUpTime)) * (PI/startUpTime);
+            ddq(5) = -maxTheta/2 * cos(seconds*(PI/startUpTime))* (PI*PI/(startUpTime*startUpTime));
+        }
+        else {
+            q(1) = maxTheta;
+            q(3) = maxTheta;
+            q(5) = maxTheta;
+        }
 
         stm.curvatureControl(q, dq, ddq);
-        std::this_thread::sleep_for(std::chrono::microseconds(int(CONTROL_PERIOD*1000000)));
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-lastTime).count();
+        sum += duration;
+        std::this_thread::sleep_for(std::chrono::microseconds(int(std::fmax(CONTROL_PERIOD*1000000 - duration - 500, 0)))); //todo: properly manage time count
     }
+    std::cout << "control loop took on average " << sum/i <<" microseconds.\n";
 }
