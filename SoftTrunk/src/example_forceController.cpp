32fef592b21af444b32eaf6c7cb11c02d27832f2
Yasu <yasu313nori@gmail.com>
2019-01-03 21:05:02 -0500

    update forceController example for new config

--- SoftTrunk/src/example_forceController.cpp
+++ SoftTrunk/src/example_forceController.cpp
@@ -2,25 +2,23 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
-
 /**
  * @file example_forceController.cpp
  * @brief This program shows an example usage of the ForceController.
  */
 void wait() { std::this_thread::sleep_for(std::chrono::milliseconds(2000)); }
-
 int main() {
-    ForceController forceController(16, 1000);
-    for (int i = 0; i < 20; i++) {
-        forceController.setSinglePressure(i % (NUM_ELEMENTS * CHAMBERS) + 10, 800);
-        wait();
-        forceController.setSinglePressure(i % (NUM_ELEMENTS * CHAMBERS) + 10, 0);
-        //forceController.setSinglePressure(i % 12, 0);
-        //forceController.setSinglePressure((i + 2) % 12, 0);
-    }
-
+  ForceController forceController(16, 1000);
+  for (int i = 0; i < 20; i++) {
+    forceController.setSinglePressure(i % (NUM_ELEMENTS*CHAMBERS)+10, 300);
     wait();
-    forceController.disconnect();
+    forceController.setSinglePressure(i % (NUM_ELEMENTS*CHAMBERS)+10, 0);
+    //forceController.setSinglePressure(i % 12, 0);
+    //forceController.setSinglePressure((i + 2) % 12, 0);
+  }
 
-    return 1;
+  wait();
+  forceController.disconnect();
+
+  return 1;
 }
