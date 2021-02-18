1c6184e0c5c60833b00b686edc8ce267098d9c82
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-02-18 22:47:18 +0100

    delete unued PID controller for valves, log to csv in real time (untested code)

--- include/mobilerack-interface/ValveController.h
+++ include/mobilerack-interface/ValveController.h
@@ -9,23 +9,19 @@
 #include "fmt/ostream.h"
 #include <mutex>
 
-/** @brief Interface for the pressure valve array. Implements an individual PID control (optional) for each valve. */
+/** @brief Interface for the pressure valve array. */
 class ValveController {
 private:
     /**
-     * @brief thread on which the PID controllers run.
+     * @brief thread on which the data is sent to Valves.
      */
     void controllerThread();
-
-    /** @brief write CSV log to log_pressure.csv */
-    void write_log();
 
     /** @brief thread runs as long as this is set to true */
     bool run;
 
     /** @brief holds the desired pressure values for each actuator */
     std::vector<int> desired_pressures;
-    std::vector<MiniPID> pid;
     MPA *mpa;
     std::thread controller_thread;
     std::mutex mtx;
@@ -33,14 +29,7 @@ private:
     const int num_valves_total = 16; /** @brief total number of valves in the Festo valve array setup */
     const std::vector<int> map;
     const int max_pressure;
-    const bool use_pid = false; /** @brief use PID control for pressure (kind of pointless since it's already done by valves */
     const bool log = true;
-
-    // variables used to save the log data
-    std::vector<double> seconds_log;
-    std::vector<std::vector<int>> sensor_pressure_log;
-    std::vector<std::vector<int>> desired_pressure_log;
-    std::chrono::high_resolution_clock::time_point logBeginTime;
 
 public:
     /**
@@ -62,5 +51,5 @@ public:
     /**
      * @brief disconnects from valve, and outputs log.
      */
-    void disconnect();
+    ~ValveController();
 };
--- src/ValveController.cpp
+++ src/ValveController.cpp
@@ -12,20 +12,6 @@ ValveController::ValveController(const char *address, const std::vector<int> &ma
     }
     fmt::print("Successfully connected to MPA at {}.\n", address);
 
-    if (use_pid) {
-        // Ziegler-Nichols method
-        double Ku = 2.6;
-        double Tu = 0.14;
-        double KP = 0.6 * Ku;
-        double KI = KP / (Tu / 2.0) * 0.002;
-        double KD = KP * Tu / 2.0 / 0.002;
-
-        for (int i = 0; i < map.size(); i++) {
-            pid.push_back(MiniPID(KP, KI, KD));
-            pid[i].setOutputLimits(20);
-            // setting a good output limit is important so as not to oscillate
-        }
-    }
     run = true;
     controller_thread = std::thread(&ValveController::controllerThread, this);
 }
@@ -40,33 +26,43 @@ void ValveController::controllerThread() {
     // sensor_pressures and output_pressures use valve IDs for easier interfacing with mpa library
     std::vector<int> sensor_pressures(num_valves_total);
     std::vector<int> output_pressures(num_valves_total);
-    logBeginTime = std::chrono::high_resolution_clock::now();
+    std::chrono::high_resolution_clock::time_point logBeginTime = std::chrono::high_resolution_clock::now();
+    std::ofstream log_file;
+    if (log){
+        std::cout << "Outputting log of ValveController to log_pressure.csv..." << std::endl;
+        log_file.open("log_pressure.csv");
+        // write first line of log
+        log_file << "time(sec)";
+        for (int i = 0; i < map.size(); ++i) {
+            log_file << fmt::format(", p_des[{}]", i); 
+        }
+        for (int i = 0; i < map.size(); ++i) {
+            log_file << fmt::format(", p_meas[{}]", i);
+        }
+        log_file << "\n";
+    }
+   
     for (int i = 0; i < num_valves_total; i++) {
         output_pressures[i] = 0;
     }
-    int valve_id;
     srl::Rate r{30};
     while (run) {
         r.sleep();
         std::lock_guard<std::mutex> lock(mtx);
         mpa->get_all_pressures(&sensor_pressures);
         for (int i = 0; i < map.size(); i++) {
-            valve_id = map[i];
-            if (use_pid)
-                output_pressures[valve_id] =
-                        desired_pressures[i] + pid[i].getOutput(sensor_pressures[valve_id], desired_pressures[i]);
-            else
-                output_pressures[valve_id] = desired_pressures[i];
             // constrain pressure value to between 0 and max_pressure
             // valve goes haywire when it tries to write negative value
-            output_pressures[valve_id] = std::max(0, std::min(output_pressures[valve_id], max_pressure));
+            output_pressures[map[i]] = std::max(0, std::min(desired_pressures[i], max_pressure));
         }
         mpa->set_all_pressures(output_pressures);
         if (log) {
-            seconds_log.push_back((double) (std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::high_resolution_clock::now() - logBeginTime).count()) / 1000);
-            sensor_pressure_log.push_back(sensor_pressures);
-            desired_pressure_log.push_back(desired_pressures);
+            log_file << (double) (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - logBeginTime).count()) / 1000.;
+            for (int valve_id: map)
+                log_file << fmt::format(", {}", sensor_pressures[valve_id]);
+            for (int valve_id: map)
+                log_file << fmt::format(", {}", desired_pressures[valve_id]);
+            log_file << "\n";
         }
     }
 
@@ -76,43 +72,11 @@ void ValveController::controllerThread() {
     }
     mpa->set_all_pressures(output_pressures);
     mpa->disconnect();
-}
-
-void ValveController::write_log(){
-    std::cout << "Outputting log of ValveController to log_pressure.csv..." << std::endl;
-    std::ofstream log_file;
-    log_file.open("log_pressure.csv");
-
-    // write first line
-    log_file << "time(sec)";
-    for (int i = 0; i < map.size(); ++i) {
-        log_file << ", p_des[" << i << "]";
-    }
-    for (int i = 0; i < map.size(); ++i) {
-        log_file << ", p_meas[" << i << "]";
-    }
-    log_file<<"\n";
-
-    // write data
-    for (int j = 0; j < seconds_log.size(); ++j) {
-        log_file << seconds_log[j];
-        // use the actuator IDs, not valve IDs when writing out
-        for (int i = 0; i < map.size(); ++i) {
-            log_file << ", " << desired_pressure_log[j][i];
-        }
-        for (int i = 0; i < map.size(); ++i) {
-            log_file << ", " << sensor_pressure_log[j][map[i]];
-        }
-        log_file << "\n";
-    }
     log_file.close();
-    std::cout<<"log_pressure.csv is output.\n";
 }
 
-void ValveController::disconnect() {
+ValveController::~ValveController() {
     run = false;
     controller_thread.join();
-    std::cout << "Stopped ValveController thread." << std::endl;
-    if (log)
-        write_log();
+    fmt::print("Stopped ValveController thread.\n");
 }
