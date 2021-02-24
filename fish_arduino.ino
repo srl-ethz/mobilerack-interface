d0add610ad0b29a6ec88c0055e58a7c3790badac
Philip <parm@ethz.ch>
2021-02-24 09:15:13 +0100

    add arduino sketch with fixed LED lighting

---
+++ fish_arduino.ino
@@ -0,0 +1,51 @@
+
+/*
+ Script to sync festo valve data with LED
+ and to record data from i2c load cell amplifier.
+*/
+
+#include "HX711.h"
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+const int LED = 13;
+
+// calibration values
+float calibration_offset = -0.1741;
+float calibration_sensitivity = -5.3781e-6;
+
+bool waitForSync = true;
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(57600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  Serial.println("Setup done");
+}
+
+void loop() {
+  handleSerial();
+  
+}
+
+void handleSerial() {
+  while (Serial.available() > 0 && waitForSync) {
+    char incomingCharacter = Serial.read();
+    switch (incomingCharacter) {
+    case 's':
+      waitForSync = false;
+      digitalWrite(LED, HIGH);
+      break;
+    }
+  }
+  // read from load cell and write to serial
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    float force = reading*calibration_sensitivity + calibration_offset;
+    Serial.println(force, 4);
+  }
+}
