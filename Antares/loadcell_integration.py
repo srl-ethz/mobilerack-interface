bfeaadcfc55e59f62bc80de367e2b95730c8a01c
Philip Arm <parm@student.ethz.ch>
2021-02-24 11:52:57 +0100

    improve logging and LED interface

--- Antares/loadcell_integration.py
+++ Antares/loadcell_integration.py
@@ -15,10 +15,11 @@ freq = 4;
 total_time = 2
 cycle_time = 1/freq
 cycle_count = total_time/cycle_time
+ser = serial.Serial('/dev/ttyACM0')
 
 # Seperate thread function for logging
-def log_function(name):
-    print('starting thread')
+def log_function(name, ser):
+    print('Start serial logging')
     # Open serial port and flush
     ser = serial.Serial('/dev/ttyACM0')
     ser.flushInput()
@@ -26,6 +27,7 @@ def log_function(name):
     string = 's'
     bytestring = string.encode()
     ser.write(bytestring)
+    starttime = time.time()
     while True:
         # Read force from Arduino
         ser_bytes = ser.readline()
@@ -33,15 +35,14 @@ def log_function(name):
         #Write data to file
         with open("force.csv","a") as f:
             writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),force_reading])
+            writer.writerow([time.time()-starttime,force_reading])
         sleep(0.1)
 
 # Create controller object
 vc = ValveController("192.168.0.100", valves, max_pressure)
 
 # Start logging thread
-print('Trying to start thread')
-log_thread = threading.Thread(target=log_function, args=(1,), daemon=True)
+log_thread = threading.Thread(target=log_function, args=(1,ser), daemon=True)
 log_thread.start()
 i = 0
 # Controller test
@@ -53,4 +54,8 @@ while i < cycle_count:
     vc.setSinglePressure(1, pressure)
     sleep(cycle_time)
     i += 1
-
+vc.disconnect()
+# turn off arduino LED
+string = 'e'
+bytestring = string.encode()
+ser.write(bytestring)
