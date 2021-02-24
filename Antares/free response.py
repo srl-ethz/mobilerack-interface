4f50afe78546da5925e2d27121f50407607db6d6
AntaresZ <zyantares@gmail.com>
2021-02-24 12:05:58 +0100

    added free response py

--- Antares/free response.py
+++ Antares/free response.py
@@ -8,24 +8,16 @@ import threading
 # File to control valves and simultaneously log load cell data from arduino on serial port
 
 # Set parameters
-valves = [15, 14] # 14: left chaber; 15: right chamber
-pressure = 200
+valves = [14, 15] # 14: left chaber; 15: right chamber
 max_pressure = 300
-freq = 4
-total_time = 2
-cycle_time = 1/freq
-cycle_count = total_time/cycle_time
 
 # Seperate thread function for logging
 def log_function(name):
     print('starting thread')
     # Open serial port and flush
     ser = serial.Serial('/dev/ttyACM0')
-    ser.write('s'.encode('ASCII'))
     ser.flushInput()
-    # ser.write("s")
     while True:
-        ser.write('s')
         # Read force from Arduino
         ser_bytes = ser.readline()
         force_reading = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
@@ -44,12 +36,30 @@ log_thread = threading.Thread(target=log_function, args=(1,), daemon=True)
 log_thread.start()
 i = 0
 # Controller test
-while i < cycle_count:
-    vc.setSinglePressure(0, pressure)
-    vc.setSinglePressure(1, 0)
-    sleep(cycle_time)
-    vc.setSinglePressure(0, 0)
-    vc.setSinglePressure(1, pressure)
-    sleep(cycle_time)
-    i += 1
+sleep(5)
+vc.setSinglePressure(0, 100)
+sleep(5)
+vc.setSinglePressure(0, 0)
+sleep(3)
+vc.setSinglePressure(0, 200)
+sleep(5)
+vc.setSinglePressure(0, 0)
+sleep(3)
+vc.setSinglePressure(0, 250)
+sleep(5)
+vc.setSinglePressure(0, 0)
+sleep(3)
+vc.setSinglePressure(1, 100)
+sleep(5)
+vc.setSinglePressure(1, 0)
+sleep(3)
+vc.setSinglePressure(1, 200)
+sleep(5)
+vc.setSinglePressure(1, 0)
+sleep(3)
+vc.setSinglePressure(1, 250)
+sleep(5)
+vc.setSinglePressure(1, 0)
+sleep(3)
+vc.disconnect()
 
