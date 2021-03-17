a04d6fff7d51d64b86445376db50fe5895879478
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-03-17 16:28:10 +0100

    update example for qualisys

--- examples_python/example_QualisysClient.py
+++ examples_python/example_QualisysClient.py
@@ -11,18 +11,17 @@ May need extra package in order to display images:
 pip3 install opencv-python
 ```
 """
-stream_image = True
-qc = QualisysClient("192.168.0.101", 2, stream_image)
+cameras = [9]
+qc = QualisysClient("192.168.0.101", 2, cameras)
 
 sleep(2)  # hacky way to wait until data is received from QTM
 for i in range(100):
     frames, timestamp = qc.getData()
     print("-"*10)
     print(f"number of frames:{len(frames)}\ttimestamp:{timestamp}\ndata:{frames}")
-    if stream_image:
-        image1, image2 = qc.getImage()
-        cv2.imshow("image1", image1)
-        cv2.imshow("image2", image2)
+    if len(cameras) > 0:
+        image = qc.getImage(0)
+        cv2.imshow("image", image)
         cv2.waitKey(1)
     sleep(0.1)
 
