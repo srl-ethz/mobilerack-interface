c1b2cea4ed1a7df5f565a0b8f3df1b273a88fae3
Benedek Forrai <benedek.forrai@gmail.com>
2023-09-05 18:13:06 +0200

    Added python examples

---
+++ examples_python/example_6DTracking.py
@@ -0,0 +1,23 @@
+from mobilerack_pybind_module import QualisysClient
+import cv2
+from time import sleep
+
+"""
+Stream frames & images from Qualisys Track Manager.
+Images are not streamed in playback mode.
+
+May need extra package in order to display images:
+```bash
+pip3 install opencv-python
+```
+"""
+cameras = [i for i in range(8)]
+qc = QualisysClient(1, cameras, "6D")
+
+sleep(2)  # hacky way to wait until data is received from QTM
+for i in range(10000):
+    frames, timestamp = qc.getData6D()
+    print("-"*10)
+    print(f"number of frames:{len(frames)}\ttimestamp:{timestamp}\ndata:{frames}")
+    print("frames: ")
+    print(frames)
